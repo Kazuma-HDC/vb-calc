317578ec799c9c13c18346800dccbbfa7c9f5ce2
Morita <hdc242313@gmail.com>
2025-05-08 14:21:57 +0900

    Form4コーディング完成

--- 計算機アプリケーション/Form4.vb
+++ 計算機アプリケーション/Form4.vb
@@ -1,3 +1,10 @@
 ﻿Public Class FrmBMI
-
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim shincyo, taijyu, BMI As Single
+        shincyo = TextBox1.Text / 100
+        taijyu = TextBox2.Text
+        BMI = taijyu / shincyo / shincyo
+        BMI = Math.Round(taijyu / shincyo / shincyo, 1)
+        Label4.Text = BMI
+    End Sub
 End Class
