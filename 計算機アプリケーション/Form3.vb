dc75d7fcb68e28704d8f476bfb463449734a62d6
Morita <hdc242313@gmail.com>
2025-05-08 14:13:52 +0900

    Form3のコーディング完成

--- 計算機アプリケーション/Form3.vb
+++ 計算機アプリケーション/Form3.vb
@@ -1,5 +1,13 @@
 ﻿Public Class FrmTra
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        Dim jyotei As Single
+        Dim katei As Single
+        Dim takasa As Single
+        Dim menseki As Single
+        jyotei = TextBox1.Text
+        katei = TextBox2.Text
+        takasa = TextBox3.Text
+        menseki = (jyotei + katei) * takasa / 2
+        Label5.Text = menseki
     End Sub
 End Class
