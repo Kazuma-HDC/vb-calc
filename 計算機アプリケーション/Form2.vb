bc60221737741c5894a6b40615f69d7193f55e72
Morita <hdc242313@gmail.com>
2025-05-08 14:06:43 +0900

    Form2のコーディング完成

--- 計算機アプリケーション/Form2.vb
+++ 計算機アプリケーション/Form2.vb
@@ -1,6 +1,10 @@
 ﻿Public Class FrmTri
     Dim menseki As Single
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim teihen As Single = TextBox1.Text
+        Dim takasa As Single = TextBox2.Text
+        menseki = teihen * takasa / 2
 
+        Label4.Text = menseki
     End Sub
 End Class
