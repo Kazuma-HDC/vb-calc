1de6eabf18198e34f2bee5b5b4d30cdcd3abe3e4
Morita <hdc242313@gmail.com>
2025-05-08 14:27:12 +0900

    Form4コード_身長の入力をmからcmに変更

--- 計算機アプリケーション/Form4.vb
+++ 計算機アプリケーション/Form4.vb
@@ -1,7 +1,7 @@
 ﻿Public Class FrmBMI
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim shincyo, taijyu, BMI As Single
-        shincyo = TextBox1.Text
+        shincyo = TextBox1.Text / 100
         taijyu = TextBox2.Text
         BMI = taijyu / shincyo / shincyo
         BMI = Math.Round(taijyu / shincyo / shincyo, 1)
